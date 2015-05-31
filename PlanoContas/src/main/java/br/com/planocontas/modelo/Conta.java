fd29725f2382676f522df2f4df898546ca52b32e
RoogerWillian <roger.nizoli@gmail.com>
2015-05-31 15:43:21 -0300

    Classe de domínio

--- PlanoContas/src/main/java/br/com/planocontas/modelo/Conta.java
+++ PlanoContas/src/main/java/br/com/planocontas/modelo/Conta.java
@@ -1,5 +1,43 @@
 package br.com.planocontas.modelo;
 
-public class Conta {
+import java.math.BigDecimal;
 
+public class Conta {
+	private Long id;
+	private String descricao;
+	private BigDecimal valor;
+	private Integer codigo;
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
 }
--- PlanoContas/src/main/java/br/com/planocontas/modelo/Subconta.java
+++ PlanoContas/src/main/java/br/com/planocontas/modelo/Subconta.java
@@ -2,4 +2,58 @@ package br.com.planocontas.modelo;
 
 public class Subconta {
 	
+	private Long id;
+	private String descricao;
+	private Integer codigo;
+	private String permissao;
+	private Conta conta;
+	private Subconta subconta;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getPermissao() {
+		return permissao;
+	}
+
+	public void setPermissao(String permissao) {
+		this.permissao = permissao;
+	}
+
+	public Conta getConta() {
+		return conta;
+	}
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+
+	public Subconta getSubconta() {
+		return subconta;
+	}
+
+	public void setSubconta(Subconta subconta) {
+		this.subconta = subconta;
+	}
 }
